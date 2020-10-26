11e005253108906b19a3ecd730f9fe6624b7cf44
Bishal <bishal.pradhan314@gmail.com>
2020-10-26 12:45:14 +0545

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, ");
+            //Console.Write("Hello, ");
+            //Console.Write("Hello, \n");
+            //Console.Write("Hello, \t\t");
+            Console.WriteLine("Welcome to C#...");
+        }
+    }
+}
