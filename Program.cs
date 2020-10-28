6a7a6f1e83c67991721dd0c991fc6676a5b7095e
Bishal <bishal.pradhan314@gmail.com>
2020-10-28 09:08:42 +0545

    DataTypes based on memory allocation ( Value DataType and Reference DataType)

--- Program.cs
+++ Program.cs
@@ -10,11 +10,49 @@ namespace Study_ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, ");
+            /*Console.WriteLine("Hello, ");
             //Console.Write("Hello, ");
             //Console.Write("Hello, \n");
             //Console.Write("Hello, \t\t");
             Console.WriteLine("Welcome to C#...");
+            */
+
+            const int a = 10;
+            string s = "Studying C#";
+
+            Console.WriteLine(a);
+            Console.WriteLine(s);
+
+
+            string name = "John";
+            int age = 30;
+
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+
+            Console.WriteLine("User name is " + name);
+            Console.WriteLine("User age is " + age);
+
+            Console.WriteLine("User name is " + name + ", User age is " + age);
+
+            Console.WriteLine("User name is " + name +  "\nUser age is " + age);
+
+            Console.WriteLine("User name is {0}\nUser age is {1}",name, age);
+
+            string name1 = Console.ReadLine();
+            int age1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("User name is {0}\nUser age is {1}", name1, age1);
+
+
+            Console.WriteLine("Enter User name");
+            string name2 = Console.ReadLine();
+            Console.WriteLine("Enter User age");
+            int age2 = int.Parse(Console.ReadLine());
+
+            Console.Clear();
+
+            Console.WriteLine("User name is {0}\nUser age is {1}", name2, age2);
+
         }
     }
 }
